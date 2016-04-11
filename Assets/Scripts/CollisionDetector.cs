451edd5dc5244665c230e3bc96c34a68b7fb9906
thakyZ <thakyz@outlook.com>
2016-04-11 11:45:39 -0700

    Stageing first round of Development Changes

---
+++ Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(PlayerMovement))]
+public class CollisionDetector : MonoBehaviour
+{
+    public LayerMask CollisionLayers;
+    public LayerMask EnemyLayers;
+    Vector3 StartPoint;
+    Vector3 Origin;
+    Vector3 DebugOrigin;
+    public int NoOfRays = 10;
+    int i;
+    RaycastHit HitInfo;
+    float LengthOfRay, DistanceBetweenRays;
+    Vector2 DirectionFactor;
+    float margin = 0.0f;
+    Ray ray;
+
+    void Start()
+    {
+        LengthOfRay = GetComponent<Collider2D>().bounds.size.y;
+    }
+
+    void Update()
+    {
+        StartPoint = new Vector3(transform.position.x + 0.5f, transform.position.y - 0.5f, transform.position.z);
+        if (IsColliding())
+        {
+            Debug.Log("Collided With \"" + HitInfo.collider.gameObject.name + "\"!");
+        }
+    }
+
+    public bool IsColliding()
+    {
+        Origin = StartPoint;
+        DistanceBetweenRays = (GetComponent<Collider2D>().bounds.size.x - 2 * margin) / (NoOfRays - 1);
+        for (i = 0; i < NoOfRays; i++)
+        {
+            ray = new Ray(Origin, GetComponent<PlayerMovement>().TargetPosition);
+
+            if (Physics.Raycast(ray, out HitInfo, LengthOfRay, CollisionLayers.value))
+            {
+                return true;
+            }
+
+            Origin += new Vector3(DistanceBetweenRays, 0, 0);
+        }
+
+        return false;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        DistanceBetweenRays = (GetComponent<Collider2D>().bounds.size.x - 2 * margin) / (NoOfRays - 1);
+        if (GetComponent<PlayerMovement>().TargetPosition.x != 0)
+        {
+            DebugOrigin = new Vector3(transform.position.x + 0.5f, transform.position.y - 1);
+        }
+        else if (GetComponent<PlayerMovement>().TargetPosition.y != 0)
+        {
+            DebugOrigin = new Vector3(transform.position.x, transform.position.y - 0.5f);
+        }
+
+        for (i = 0; i < NoOfRays; i++)
+        {
+            if (GetComponent<PlayerMovement>().TargetPosition.x != 0)
+            {
+
+                Gizmos.DrawRay(DebugOrigin, GetComponent<PlayerMovement>().TargetPosition * 10);
+
+                DebugOrigin += new Vector3(0, DistanceBetweenRays, 0);
+                }
+                else if (GetComponent<PlayerMovement>().TargetPosition.y != 0)
+                {
+
+                Gizmos.DrawRay(DebugOrigin, GetComponent<PlayerMovement>().TargetPosition * 10);
+
+                DebugOrigin += new Vector3(DistanceBetweenRays, 0, 0);
+            }
+        }
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(new Vector3(transform.position.x + 0.5f, transform.position.y - 0.5f, 0), new Vector3(transform.position.x + 0.5f + GetComponent<PlayerMovement>().TargetPosition.x * 15, transform.position.y - 0.5f + GetComponent<PlayerMovement>().TargetPosition.y * 15, 0));
+    }
+}
+
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,167 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    public int SizeOfTiles;
+    public LayerMask CollisionLayer;
+
+    [HideInInspector]
+    public Vector3 TargetPosition;
+    private bool canMove = true;
+
+    private Animator playerAnimator;
+
+    void Start()
+    {
+        playerAnimator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            // Set Animation to Idle.
+            playerAnimator.SetBool("Moving", false);
+
+        }
+        if (Input.GetAxis("Vertical") > 0)
+        {
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+        }
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            if (Input.GetAxis("Vertical") > 0 && canMove == true)
+            {
+                if (!Physics2D.Linecast(new Vector2(transform.position.x + 0.5f, transform.position.y - 0.5f), new Vector2(transform.position.x + 0.5f, transform.position.y - 0.0f), CollisionLayer))
+                {
+                    canMove = false;
+                    StartCoroutine(MoveInGrid((int)transform.position.x, (int)transform.position.y + SizeOfTiles));
+                }
+            }
+            else if (Input.GetAxis("Vertical") < 0 && canMove == true)
+            {
+                if (!Physics2D.Linecast(new Vector2(transform.position.x + 0.5f, transform.position.y - 0.5f), new Vector2(transform.position.x + 0.5f, transform.position.y - 1.0f), CollisionLayer))
+                {
+                    canMove = false;
+                    StartCoroutine(MoveInGrid((int)transform.position.x, (int)transform.position.y - SizeOfTiles));
+                }
+            }
+            if (Input.GetAxis("Horizontal") > 0 && canMove == true)
+            {
+                if (!Physics2D.Linecast(new Vector2(transform.position.x + 0.5f, transform.position.y - 0.5f), new Vector2(transform.position.x + 1.0f, transform.position.y - 0.5f), CollisionLayer))
+                {
+                    canMove = false;
+                    StartCoroutine(MoveInGrid((int)transform.position.x + SizeOfTiles, (int)transform.position.y));
+                }
+            }
+            if (Input.GetAxis("Horizontal") < 0 && canMove == true)
+            {
+                if (!Physics2D.Linecast(new Vector2(transform.position.x + 0.5f, transform.position.y - 0.5f), new Vector2(transform.position.x - 0.0f, transform.position.y - 0.5f), CollisionLayer))
+                {
+                    canMove = false;
+                    StartCoroutine(MoveInGrid((int)transform.position.x - SizeOfTiles, (int)transform.position.y));
+                }
+            }
+        }
+    }
+
+    IEnumerator MoveInGrid(int x, int y)
+    {
+        while (transform.position.x != x || transform.position.y != y)
+        {
+            //moving x forward
+            if (transform.position.x < x)
+            {
+                //moving the point by speed 
+                TargetPosition.x = Speed * Time.deltaTime;
+                //check if the point goes more than it should go and if yes clamp it back
+                if (TargetPosition.x + transform.position.x > x)
+                {
+                    TargetPosition.x = x - transform.position.x;
+                }
+                // Set Animation Moving Right
+                playerAnimator.SetInteger("Direction", 2);
+                playerAnimator.SetBool("Moving", true);
+            }
+            //moving x backward
+            else if (transform.position.x > x)
+            {
+                //moving the point by speed 
+                TargetPosition.x = -Speed * Time.deltaTime;
+                //check if the point goes more than it should go and if yes clamp it back
+                if (TargetPosition.x + transform.position.x < x)
+                {
+                    TargetPosition.x = -(transform.position.x - x);
+                }
+                // Set Animation Moving Left
+                playerAnimator.SetInteger("Direction", 4);
+                playerAnimator.SetBool("Moving", true);
+            }
+            else //x is unchanged so should be 0 in translate function
+            {
+                TargetPosition.x = 0;
+            }
+            //moving y forward
+            if (transform.position.y < y)
+            {
+                //moving the point by speed 
+                TargetPosition.y = Speed * Time.deltaTime;
+                //check if the point goes more than it should go and if yes clamp it back
+                if (TargetPosition.y + transform.position.y > y)
+                {
+                    TargetPosition.y = y - transform.position.y;
+                }
+                // Set Animation Moving Up
+                playerAnimator.SetInteger("Direction", 1);
+                playerAnimator.SetBool("Moving", true);
+            }
+            //moving y backward
+            else if (transform.position.y > y)
+            {
+                //moving the point by speed 
+                TargetPosition.y = -Speed * Time.deltaTime;
+                //check if the point goes more than it should go and if yes clamp it back
+                if (TargetPosition.y + transform.position.y < y)
+                {
+                    TargetPosition.y = -(transform.position.y - y);
+                }
+                // Set Animation Moving Down
+                playerAnimator.SetInteger("Direction", 3);
+                playerAnimator.SetBool("Moving", true);
+            }
+            else //y is unchanged so it should be zero
+            {
+                TargetPosition.y = 0;
+            }
+
+            TargetPosition.z = 0;
+
+            transform.Translate(TargetPosition);
+            yield return 0;
+        }
+        //the work is ended now congratulation
+        canMove = true;
+        TargetPosition = Vector2.zero;
+    }
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(new Vector3(transform.position.x + 0.5f, transform.position.y - 0.5f, -2), new Vector3(transform.position.x + 0.5f, transform.position.y - 1.0f, -2));
+        Gizmos.DrawLine(new Vector3(transform.position.x + 0.5f, transform.position.y - 0.5f, -2), new Vector3(transform.position.x + 0.5f, transform.position.y - 0.0f, -2));
+        Gizmos.DrawLine(new Vector3(transform.position.x + 0.5f, transform.position.y - 0.5f, -2), new Vector3(transform.position.x - 0.0f, transform.position.y - 0.5f, -2));
+        Gizmos.DrawLine(new Vector3(transform.position.x + 0.5f, transform.position.y - 0.5f, -2), new Vector3(transform.position.x + 1.0f, transform.position.y - 0.5f, -2));
+    }
+}
